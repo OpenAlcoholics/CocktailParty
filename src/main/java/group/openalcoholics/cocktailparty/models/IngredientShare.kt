4fc527d8e6bca7c962e33f6df11df54ca19a36af
Björn Petersen <pheasn@gmail.com>
2018-09-22 17:14:48 +0200

    Exclude rank from IngredientShare equality

--- src/main/java/group/openalcoholics/cocktailparty/models/IngredientShare.kt
+++ src/main/java/group/openalcoholics/cocktailparty/models/IngredientShare.kt
@@ -6,5 +6,23 @@ data class IngredientShare(
     val ingredientId: Int,
     val share: Int,
     @JsonIgnore
-    val rank: Int? = null
-)
+    val rank: Int? = null) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as IngredientShare
+
+        if (ingredientId != other.ingredientId) return false
+        if (share != other.share) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = ingredientId
+        result = 31 * result + share
+        return result
+    }
+}
