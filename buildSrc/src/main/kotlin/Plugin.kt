a6f18a29b40decea657e6846d471bbc1e434a118
Björn Petersen <pheasn@gmail.com>
2019-03-29 12:30:04 +0100

    Update Plugin versions

--- buildSrc/src/main/kotlin/Plugin.kt
+++ buildSrc/src/main/kotlin/Plugin.kt
@@ -1,6 +1,6 @@
 object Plugin {
     const val KOTLIN = "1.3.21"
-    const val DOKKA = "0.9.17"
+    const val DOKKA = "0.9.18"
     const val FLYWAY = "5.2.4"
-    const val VERSIONS = "0.20.0"
+    const val VERSIONS = "0.21.0"
 }
