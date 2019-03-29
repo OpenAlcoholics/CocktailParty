39d9c23bfa8eaf411199063ddcf0aca7adb241fa
Björn Petersen <pheasn@gmail.com>
2019-03-29 12:34:16 +0100

    Update dependency versions

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -1,22 +1,22 @@
 object Lib {
     const val KOTLIN = Plugin.KOTLIN
-    const val SLF4J = "1.7.25"
-    const val KOTLIN_LOGGING = "1.6.22"
-    const val CFG4K = "0.9.0"
-    const val GUAVA = "27.0.1-jre"
+    const val SLF4J = "1.7.26"
+    const val KOTLIN_LOGGING = "1.6.25"
+    const val CFG4K = "0.9.3"
+    const val GUAVA = "27.1-jre"
     const val GUICE = "4.2.2"
 
-    const val VERTX = "3.6.2"
+    const val VERTX = "3.6.3"
     const val VERTX_GUICE = "2.3.1"
 
     const val FLYWAY = Plugin.FLYWAY
-    const val JDBI = "3.5.1"
-    const val HIKARI_CP = "3.2.0"
+    const val JDBI = "3.6.0"
+    const val HIKARI_CP = "3.3.1"
     const val JACKSON = "2.9.8"
     const val POSTGRESQL = "42.2.5"
 
-    const val JUNIT = "5.3.1"
-    const val JUNIT_GUICE = "1.1.0"
-    const val MOCK_K = "1.9"
-    const val ASSERT_J = "3.11.1"
+    const val JUNIT = "5.4.1"
+    const val JUNIT_GUICE = "1.1.1"
+    const val MOCK_K = "1.9.3"
+    const val ASSERT_J = "3.12.2"
 }
