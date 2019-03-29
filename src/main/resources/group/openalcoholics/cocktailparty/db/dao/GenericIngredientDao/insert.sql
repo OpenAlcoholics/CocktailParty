6eb9ee8fb61562975ee480f8bc3e4dbfd5e6f925
Björn Petersen <pheasn@gmail.com>
2019-03-29 14:08:38 +0100

    Fix GenericIngredient update and insert

--- src/main/resources/group/openalcoholics/cocktailparty/db/dao/GenericIngredientDao/insert.sql
+++ src/main/resources/group/openalcoholics/cocktailparty/db/dao/GenericIngredientDao/insert.sql
@@ -1,2 +1,2 @@
 INSERT INTO generic_ingredient (name, description, image_link, is_alcoholic)
-VALUES (:entity.name, :entity.description, :entity.imageLink, :entity.alcoholic)
+VALUES (:entity.name, :entity.description, :entity.imageLink, :entity.isAlcoholic)
--- src/main/resources/group/openalcoholics/cocktailparty/db/dao/GenericIngredientDao/update.sql
+++ src/main/resources/group/openalcoholics/cocktailparty/db/dao/GenericIngredientDao/update.sql
@@ -2,5 +2,5 @@ UPDATE generic_ingredient
 SET name = :entity.name,
     description = :entity.description,
     image_link = :entity.imageLink,
-    is_alcoholic = :entity.alcoholic
+    is_alcoholic = :entity.isAlcoholic
 WHERE id = :entity.id
